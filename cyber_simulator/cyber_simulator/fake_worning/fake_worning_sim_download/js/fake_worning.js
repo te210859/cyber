5553cd1fa19d7b5bb8e219ff90c42bf79a2b0a8e
Iwano Ichiro <tejc210731@gmail.com>
2024-11-29 16:33:05 +0900

    偽広告(仮)

--- cyber_simulator/cyber_simulator/fake_worning/fake_worning_sim_download/js/fake_worning.js
+++ cyber_simulator/cyber_simulator/fake_worning/fake_worning_sim_download/js/fake_worning.js
@@ -1,9 +1,7 @@
 // scripts.js
-
 // ページがロードされた後に実行するコード
 window.onload = function() {
-    console.log("ページがロードされました!");
-    
+    console.log("ページがロードされました!"); 
     // ナビゲーションリンクをクリックすると、ページのセクションにスクロール
     const navLinks = document.querySelectorAll('nav ul li a');
     navLinks.forEach(link => {
